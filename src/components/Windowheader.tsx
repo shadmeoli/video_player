fad6c58f50dcff067f96c16da1c7500d4379ed61
shadmeoli <shadrack@shopzetu.com>
2024-01-20 20:34:05 +0300

    🔖 react icons addition

--- src/components/Windowheader.tsx
+++ src/components/Windowheader.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+// import React from 'react'
+import { useNavigate } from "react-router-dom";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+
+
 
 export default function Windowheader() {
+  const navigate = useNavigate();
+
+  function goBack() {
+    navigate("/")
+  }
+
   return (
     <div className='w-full h-14 bg-opacity-40 absolute top-0 z-10 bg-black backdrop-blur-xl flex flex-row justify-between items-center font-primary px-10'>
+      <button onClick={goBack}>
+        <AiOutlineArrowLeft />
+      </button>
       <div className='flex flex-col items-start justify-center'>
         <h1 className='font-bold text-white'>Picky Blinders</h1>
         <h1 className='font-light text-white text-sm'>Season 1 (Eps 1)</h1>
