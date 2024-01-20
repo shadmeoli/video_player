5874939c264dda8ce91373b456d5c72ae6f3580b
shadmeoli <shadrack@shopzetu.com>
2024-01-20 20:49:07 +0300

    🎉 player screen

--- src/components/Windowheader.tsx
+++ src/components/Windowheader.tsx
@@ -4,17 +4,20 @@ import { AiOutlineArrowLeft } from "react-icons/ai";
 
 
 
-export default function Windowheader() {
+export default function Windowheader({ title }) {
+
   const navigate = useNavigate();
 
+  console.log(`Now playing : ${title}`)
   function goBack() {
     navigate("/")
   }
 
   return (
-    <div className='w-full h-14 bg-opacity-40 absolute top-0 z-10 bg-black backdrop-blur-xl flex flex-row justify-between items-center font-primary px-10'>
-      <button onClick={goBack}>
+    <div className='w-full h-14 bg-opacity-20 absolute top-0 z-10 bg-black backdrop-blur-xl flex flex-row justify-between items-center font-primary px-10'>
+      <button className="flex flex-row space-x-2 items-center hover:bg-white/30 p-2 transition duration-300 ease-in-out rounded-full" onClick={goBack}>
         <AiOutlineArrowLeft />
+        <h1 className="font-bold">Back</h1>
       </button>
       <div className='flex flex-col items-start justify-center'>
         <h1 className='font-bold text-white'>Picky Blinders</h1>
