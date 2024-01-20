c0c98e2e1048d50712233b4b2796284846e39912
shadmeoli <shadrack@shopzetu.com>
2024-01-20 20:42:11 +0300

    🎉 simple video play back feature with the HTML5 video tag

--- src/screens/Player.tsx
+++ src/screens/Player.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Logo from "../assets/ndc.png";
 // import { countReducer } from "../store/homeStore";
 // import { useNavigate } from "react-router-dom";
+import video from "../assets/test.webm"
 
 import Track from "../components/Track";
 import Windowheader from "../components/Windowheader";
@@ -15,19 +16,10 @@ export default function Player() {
     <>
       <div className="h-screen w-screen">
         <Windowheader />
-        <div className="w-full h-full hover:h-[90%] flex flex-row items-center justify-center font-primary bg-gray-200 transition duration-300 ease-in-out">
-          <div className="flex flex-col items-center space-y-4">
-            <img src={Logo} alt="" className="h-[80%] duration-500" />
-            <h1 className="font-semibold">This is not a VLC alternative</h1>
-            <p>This is just a video player toy project</p>
-            <div className="flex flex-row space-x-2">
-              <button onClick={() => setCounter(counter + 1)} className="p-2 bg-cyan-200 text-cyan-600 font-bold rounded-full">Browse local</button>
-              <button onClick={() => setCounter(0)} className="p-2 bg-cyan-200 text-cyan-600 font-bold rounded-full">RESET</button>
-            </div>
-            <p className="font-bold text-cyan-600">{counter}</p>
-          </div>
+        <div className="w-full h-full flex flex-row items-center justify-center font-primary bg-gray-200 transition duration-300 ease-in-out">
+          <video src={video} className="h-full w-full" controls>
+          </video>
         </div>
-        <Track />
       </div>
     </>
   );
