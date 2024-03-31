6a4380ba42abf4334bb0f71c2eaff9879e1a5395
shadmeoli <shadrack@shopzetu.com>
2024-03-31 20:16:05 +0300

    update: player title clean up

--- src/components/Windowheader.tsx
+++ src/components/Windowheader.tsx
@@ -13,15 +13,18 @@ export default function Windowheader(props: any) {
   }
 
   return (
-    <div className='w-full h-14 bg-opacity-20 absolute top-0 z-10 bg-black backdrop-blur-xl flex flex-row justify-between items-center font-primary px-10'>
-      <button className="flex flex-row space-x-2 items-center hover:bg-white/30 p-2 transition duration-300 ease-in-out rounded-full" onClick={goBack}>
+    <div className="w-full h-14 bg-opacity-20 absolute top-0 z-10 bg-black backdrop-blur-xl flex flex-row justify-between items-center font-primary px-10">
+      <button
+        className="flex flex-row space-x-2 items-center hover:bg-white/30 p-2 transition duration-300 ease-in-out rounded-full"
+        onClick={goBack}
+      >
         <AiOutlineArrowLeft />
         <h1 className="font-bold">Back</h1>
       </button>
-      <div className='flex flex-col items-start justify-center'>
+      {/* <div className='flex flex-col items-start justify-center'>
         <h1 className='font-bold text-white'>Picky Blinders</h1>
         <h1 className='font-light text-white text-sm'>Season 1 (Eps 1)</h1>
-      </div>
+      </div> */}
     </div>
-  )
+  );
 };
